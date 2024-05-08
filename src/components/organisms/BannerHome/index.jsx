9a32d3a0b49a919e57651cfcf8439af091916fbf
ikhbaldwiyantoro <ikhbal.d@kiranatama.com>
2024-05-08 13:29:36 +0700

    adjust layout home description max width

--- src/components/organisms/BannerHome/index.jsx
+++ src/components/organisms/BannerHome/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.scss";
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
-import Banner from "/assets/images/banner.png";
+import Banner from "/assets/images/banner-home.png";
 import { FcGoogle } from "react-icons/fc";
 
 const BannerHome = () => {
@@ -13,29 +13,31 @@ const BannerHome = () => {
             <p className="welcome">
               Selamat Datang di Pendaftaran Calon Siswa BPK PENABUR
             </p>
-            <p className="sub-title">
-              Pendaftaran ini hanya untuk
-              <span className="info-title mx-2">
-                Siswa NON BPK PENABUR Jakarta
-              </span>
-              atau belum pernah mendaftar sama sekali di BPK PENABUR JAKARTA.
-            </p>
-            <p className="desc-banner">
-              Terima kasih telah memilih sekolah kami sebagai mitra dalam
-              pencapaian impian pendidikan. Kami berkomitmen untuk menyediakan
-              pengalaman belajar yang luar biasa untuk setiap siswa.
-              Bersama-sama, kami akan membangun masa depan yang gemilang bagi
-              setiap siswa.
-            </p>
-            <div className="login-space">
-              <p className="login-title">Silakan masuk untuk melanjutkan</p>
-              <Button
-                size="md"
-                variant="outline-primary"
-                className="login-google-btn"
-              >
-                <FcGoogle size={24} /> Login Sekarang
-              </Button>
+            <div className="desc-wrapper">
+              <p className="sub-title">
+                Pendaftaran ini hanya untuk
+                <span className="info-title mx-2">
+                  Siswa NON BPK PENABUR Jakarta
+                </span>
+                atau belum pernah mendaftar sama sekali di BPK PENABUR JAKARTA.
+              </p>
+              <p className="desc-banner">
+                Terima kasih telah memilih sekolah kami sebagai mitra dalam
+                pencapaian impian pendidikan. Kami berkomitmen untuk menyediakan
+                pengalaman belajar yang luar biasa untuk setiap siswa.
+                Bersama-sama, kami akan membangun masa depan yang gemilang bagi
+                setiap siswa.
+              </p>
+              <div className="login-space">
+                <p className="login-title">Silakan masuk untuk melanjutkan</p>
+                <Button
+                  size="md"
+                  variant="outline-primary"
+                  className="login-google-btn"
+                >
+                  <FcGoogle size={24} /> Login Sekarang
+                </Button>
+              </div>
             </div>
           </Col>
           <Col md="6" sm="12">
